4020982d9369e9a2a420741239744d43cd9aef48
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-09-20 21:11:24 +0200

    add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 // import Container from './components/Container';
 import Header from './components/Header/Header.jsx';
+import Container from './components/Container/Container.jsx';
+
 export default function App() {
   return (
-    <Header>
-      <h1>2</h1>
-    </Header>
+    <Container>
+      <Header></Header>
+    </Container>
   );
 }
