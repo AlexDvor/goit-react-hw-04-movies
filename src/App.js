84a6a5ebb4186ee50f9a940298fe4b29122a4b97
Oleksii Matviichuk <alexmatvichuk@gmail.com>
2021-09-23 16:04:17 +0200

    added movieList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { Route, Switch } from 'react-router';
+// components
 import Header from './components/Header/Header';
 import Container from './components/Container/Container.jsx';
+// pages
+import MovieList from './pages/MovieList';
 
 function App() {
   return (
     <>
       <Container>
         <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <MovieList></MovieList>
+          </Route>
+        </Switch>
       </Container>
     </>
   );
---
+++ src/pages/MovieList.jsx
@@ -0,0 +1,13 @@
+export default function MovieList() {
+  return (
+    <>
+      <h2>Trending Today</h2>
+      <ul>
+        <li>sdfsdfsd</li>
+        <li>sdfsdfsdsfsdfsdf</li>
+        <li>sdfsdfsdsfsdfsdfsfsdfsd</li>
+        <li>sdfsdfsdsfsdfsdfsfsdfsdsdfsdf</li>
+      </ul>
+    </>
+  );
+}
